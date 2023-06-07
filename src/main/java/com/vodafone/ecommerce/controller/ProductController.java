efe1694be9f9749983ba12c3a05c1259b6faa3c7
mohamedmahmoud849 <mohamed2000.2023@gmail.com>
2023-06-07 21:30:22 +0200

    updating make order functions to check first in stock

--- src/main/java/com/vodafone/ecommerce/controller/ProductController.java
+++ src/main/java/com/vodafone/ecommerce/controller/ProductController.java
@@ -1,7 +1,6 @@
 package com.vodafone.ecommerce.controller;
 
-import com.vodafone.ecommerce.service.productService;
-import jakarta.servlet.http.HttpSession;
+import com.vodafone.ecommerce.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
@@ -12,14 +11,14 @@ import org.springframework.web.multipart.MultipartFile;
 @RequiredArgsConstructor
 public class ProductController extends BaseController {
 
-    private final productService productService;
+    private final ProductService productService;
 
     @GetMapping("/add_product")
-    public String addNewProduct(){
+    public String showAddItemForm(){
         return "new_product";
     }
     @PostMapping("/add_product")
-    public String addNewProductP(@RequestParam("file") MultipartFile file,
+    public String addNewProduct(@RequestParam("file") MultipartFile file,
                                 @RequestParam("name") String name,
                                 @RequestParam("category") String category,
                                 @RequestParam("price") Integer price,
@@ -28,12 +27,8 @@ public class ProductController extends BaseController {
         return "redirect:/";
     }
     @RequestMapping("/items/{id}")
-    public String itemPage(@PathVariable Long id, Model model){
+    public String showItemPage(@PathVariable Long id, Model model){
         model.addAttribute("item",productService.getProductById(id));
-        /*httpSession.setAttribute("product", productService.getProductById(id));
-        httpSession.setAttribute("ses_text", String.valueOf("lol"));
-        model.addAttribute("session_obj",httpSession.getAttribute("product"));
-        model.addAttribute("session_text",httpSession.getAttribute("ses_text"));*/
         return "item_page";
     }
 
--- src/main/java/com/vodafone/ecommerce/service/ProductService.java
+++ src/main/java/com/vodafone/ecommerce/service/ProductService.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class productService {
+public class ProductService {
 
     private final ProductRepo productRepo;
 
