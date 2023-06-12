dbd4d516ca407bb14abc9f0ca3258e425f5b9600
mohamedmahmoud849 <mohamed2000.2023@gmail.com>
2023-06-12 13:46:44 +0200

    adding error handling classes

---
+++ src/main/java/com/vodafone/ecommerce/model/ErrorDetails.java
@@ -0,0 +1,12 @@
+package com.vodafone.ecommerce.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ErrorDetails {
+    private String code;
+    private String message;
+    private String url;
+}
