a03f6cf6a015d7b67d9597852da4d9126353c348
mohamedmahmoud849 <mohamed2000.2023@gmail.com>
2023-06-13 06:18:11 +0200

    adding mail service feature verify and reset password and make reset password page

--- src/main/java/com/vodafone/ecommerce/service/MailService.java
+++ src/main/java/com/vodafone/ecommerce/service/MailService.java
@@ -15,7 +15,7 @@ import com.vodafone.ecommerce.model.Mail;
 public class MailService {
     private final JavaMailSender javaMailSender;
 
-    public void sendMail(Mail mail) throws MessagingException {
+    public void sendVerifyMail(String email,Long id) throws MessagingException {
         MimeMessage mimeMessage = javaMailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
         String htmlMsg = "<!DOCTYPE html>\n" +
@@ -25,7 +25,7 @@ public class MailService {
                 "<h1 style=\"alignment:centre; text-align: center;\">Please Verify Your Email</h1>\n" +
                 "<br>\n" +
                 "<div style=\"margin-left:400px\">\n" +
-                "<a href=\"http://localhost:8090/\">\n" +
+                "<a href=\"http://localhost:8090/verify/"+id+"\">\n" +
                 "<button type=\"button\" style=\"position: absolute;\n" +
                 "    top: 30%;\n" +
                 "    left: 50%;\n" +
@@ -42,15 +42,55 @@ public class MailService {
                 "  display: inline-block;\n" +
                 "  font-size: 16px;\n" +
                 "  border-radius: 4px;\n" +
-                "\" >Click Me!</button>\n" +
+                "\" >Verify</button>\n" +
                 "</a>\n" +
                 "</div>\n" +
                 "</body>\n" +
                 "</html>";
 //mimeMessage.setContent(htmlMsg, "text/html"); /** Use this or below line **/
         helper.setText(htmlMsg, true); // Use this or above line.
-        helper.setTo("someone@abc.example");
-        helper.setSubject("This is the test message for testing gmail smtp server using spring mail");
+        helper.setTo(email);
+        helper.setSubject("verify Email");
+        helper.setFrom("abc@gmail.com");
+        javaMailSender.send(mimeMessage);
+
+    }
+    public void sendResetPasswordMail(String email,Long id) throws MessagingException {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
+        String htmlMsg = "<!DOCTYPE html>\n" +
+                "<html>\n" +
+                "<body>\n" +
+                "\n" +
+                "<h1 style=\"alignment:centre; text-align: center;\">Please Verify Your Email</h1>\n" +
+                "<br>\n" +
+                "<div style=\"margin-left:400px\">\n" +
+                "<a href=\"http://localhost:8090/reset/"+id+"\">\n" +
+                "<button type=\"button\" style=\"position: absolute;\n" +
+                "    top: 30%;\n" +
+                "    left: 50%;\n" +
+                "    margin-top: -50px;\n" +
+                "    margin-left: -50px;\n" +
+                "    width: 150px;\n" +
+                "    height: 70px;\n" +
+                "    background-color: #4CAF50; /* Green */\n" +
+                "  border: none;\n" +
+                "  color: white;\n" +
+                "  padding: 15px 32px;\n" +
+                "  text-align: center;\n" +
+                "  text-decoration: none;\n" +
+                "  display: inline-block;\n" +
+                "  font-size: 16px;\n" +
+                "  border-radius: 4px;\n" +
+                "\" >Reset Password</button>\n" +
+                "</a>\n" +
+                "</div>\n" +
+                "</body>\n" +
+                "</html>";
+
+        helper.setText(htmlMsg, true); // Use this or above line.
+        helper.setTo(email);
+        helper.setSubject("reset Password");
         helper.setFrom("abc@gmail.com");
         javaMailSender.send(mimeMessage);
 
